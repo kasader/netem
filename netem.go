bcf266f78fffba39fe80e7e169411bde56ca1a10
kasader <casada980@gmail.com>
2026-02-04 20:43:17 +0900

    Add package API comment

---
+++ netem.go
@@ -0,0 +1,6 @@
+// Package netem provides network emulation wrappers for the [net.PacketConn]
+// and [net.Conn] interfaces; it allows for dynamic thread-safe configuration
+// of connection bandwidth, latency, jitter, and packet loss via indirection.
+package netem
+
+import _ "net"
