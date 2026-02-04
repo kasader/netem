dd8807667e8fbbb0d12f848c581ac5bedfd79066
kasader <casada980@gmail.com>
2026-02-04 23:47:14 +0900

    Change default JitterFunc

--- emulation.go
+++ emulation.go
@@ -41,6 +41,8 @@ type Latency interface {
 	Duration() time.Duration
 }
 
+// TODO: Add sine-wave latency func, etc. more utilities!
+
 // LatencyFunc enables a simple function to satisfy the [Latency] interface.
 type LatencyFunc func() time.Duration
 
@@ -72,18 +74,37 @@ type JitterFunc func() time.Duration
 
 func (f JitterFunc) Duration() time.Duration { return f() }
 
-// StaticJitter returns a constant jitter variance.
-func StaticJitter(d time.Duration) Jitter { return JitterFunc(func() time.Duration { return d }) }
+// RandomJitter returns a jitter function that selects a random value
+// uniformly distributed in the range [-amplitude, +amplitude].
+//
+// For example, RandomJitter(10*time.Millisecond) will return a duration
+// randomly chosen between -10ms and +10ms.
+func RandomJitter(amplitude time.Duration) JitterFunc {
+	return JitterFunc(func() time.Duration {
+		if amplitude == 0 {
+			return 0
+		}
+		n := int64(amplitude)
+		delta := rand.Int64N(2 * n)
+		return time.Duration(delta - n)
+	})
+}
 
 // JitterVar is a thread-safe, mutable [Jitter] provider.
 // It allows you to change the jitter of a running simulation.
+//
+// Uses the [RandomJitter] policy. For other policies, please implement a
+// custom LossVar implementation.
 type JitterVar struct{ val atomic.Int64 }
 
 // Set updates the jitter safely.
 func (v *JitterVar) Set(d time.Duration) { v.val.Store(int64(d)) }
 
 // Duration implements the [Latency] interface.
-func (v *JitterVar) Duration() time.Duration { return time.Duration(v.val.Load()) }
+func (v *JitterVar) Duration() time.Duration {
+	amplitude := time.Duration(v.val.Load())
+	return RandomJitter(amplitude)()
+}
 
 // --- Loss
 
@@ -100,9 +121,9 @@ func (f LossFunc) Drop() bool { return f() }
 
 // RandomLoss returns a function that drops datagrams with probability rate (0.0 to 1.0).
 func RandomLoss(rate float64) LossFunc {
-	return func() bool {
+	return LossFunc(func() bool {
 		return rand.Float64() < rate
-	}
+	})
 }
 
 // LossVar is a thread-safe, mutable [Loss] provider.
